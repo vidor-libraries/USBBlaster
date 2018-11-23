e6f80848d98b2ff00a1e158dbe09907a5fc3e12d
Martino Facchin <m.facchin@arduino.cc>
2018-11-23 16:50:13 +0100

    Comment enableFpgaClock() function in example

--- examples/USB_Blaster/USB_Blaster.ino
+++ examples/USB_Blaster/USB_Blaster.ino
@@ -2,8 +2,11 @@
 
 static bool activityLed = true;
 
+extern void enableFpgaClock();
+
 void setup() {
   USBBlaster.begin(activityLed);
+  // also start the 48MHz clock feed for the FPGA, in case we need to run the bitstream and need this clock
   enableFpgaClock();
 }
 
